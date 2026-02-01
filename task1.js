976d84cf70b960bc4ee759d43bacf1bda1afcab5
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-02-01 20:12:36 +0600

    Complete Task1

---
+++ task1.js
@@ -0,0 +1,11 @@
+/**
+ * Take four parameters. Multiply the four numbers and then return the result
+ */
+
+function multiply(a,b,c,d){
+     let mult = a*b*c*d;
+     return mult;
+}
+
+const fourNumber = multiply(10,20,30,40);
+console.log("Four Numbers total Multiply: ",fourNumber);
